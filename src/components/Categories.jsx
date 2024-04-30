8a0681505c877d896bc16da3d026f0e1cecffffe
Khalidulla <daulethalidulla03@gmail.com>
2024-04-30 15:38:27 +0500

    n

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
 function Categories() {
-  const [] = React.useState();
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  
+  const onClickCategory = () => {
+    setActiveIndex(3)
+  }
 
   return (
       <div className="categories">
         <ul>
-          <li className="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
+          <li onClick={onClickCategory} className={activeIndex === 0 ? 'active' :  ''}>Все</li>
+          <li onClick={onClickCategory} className={activeIndex === 1 ? 'active' :  ''}>Мясные</li>
+          <li onClick={onClickCategory} className={activeIndex === 2 ? 'active' :  ''}>Вегетарианская</li>
+          <li onClick={onClickCategory} className={activeIndex === 3 ? 'active' :  ''}>Гриль</li>
+          <li onClick={onClickCategory} className={activeIndex === 4 ? 'active' :  ''}>Острые</li>
+          <li onClick={onClickCategory} className={activeIndex === 5 ? 'active' :  ''}>Закрытые</li>
         </ul>
       </div>
     );
